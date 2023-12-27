9f7580467fdaac0e8ba749f812dd9833eaf7cb95
mack7 <balajirk10@gmail.com>
2023-12-27 20:04:29 +0530

    sign up page is added

--- lib/ui/screen/login/login_screen.dart
+++ lib/ui/screen/login/login_screen.dart
@@ -1,9 +1,11 @@
 import 'package:dr_news/ui/screen/home/home_screen.dart';
+import 'package:dr_news/ui/screen/login/sign_up_screen.dart';
+import 'package:dr_news/ui/screen/login/user_details.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../../common/form_validation.dart';
-
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -14,9 +16,6 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
-
-  final String _password = 'Dinesh1@';
-  final String _mail = 'dhineshramadoss10@gmail.com';
 
   final Validation validation = Validation();
 
@@ -46,7 +45,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 children: [
                   const SizedBox(height: 20),
                   Text(
-                    'DRNews',
+                    'Login',
                     style: TextStyle(
                       fontSize: 40,
                       fontWeight: FontWeight.w900,
@@ -54,7 +53,6 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                   ),
                   SizedBox(height: size.size.height * 0.17),
-
                   TextFormField(
                     keyboardType: TextInputType.emailAddress,
                     controller: email,
@@ -118,19 +116,12 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                   ),
                   const SizedBox(height: 20),
-
                   SizedBox(
                     width: double.infinity,
                     height: 65,
                     child: FilledButton(
                       onPressed: () {
-                        // pushNewsScreen();
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const NewsScreen(),
-                          ),
-                        );
+                        pushNewsScreen(context);
                       },
                       child: const Text('LOGIN WITH EMAIL'),
                     ),
@@ -148,7 +139,14 @@ class _LoginScreenState extends State<LoginScreen> {
                       ),
                       const SizedBox(width: 5),
                       InkWell(
-                        onTap: () {},
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const SignUpScreen(),
+                            ),
+                          );
+                        },
                         borderRadius: BorderRadius.circular(30),
                         child: Text(
                           'Sign up',
@@ -169,34 +167,44 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  void pushNewsScreen() {
+  void pushNewsScreen(BuildContext context) {
     if (_formKey.currentState!.validate()) {
-      if (password.text == _password && email.text == _mail) {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => const NewsScreen(),
-          ),
-        );
-      } else {
-        showDialog<String>(
-          barrierLabel: 'What is this',
-          context: context,
-          builder: (context) {
-            return CupertinoAlertDialog(
-              title: const Text('Login failed'),
-              content: const Text(
-                  '''Your username or password are incorrect.\nPlease try again'''),
-              actions: [
-                CupertinoButton(
-                  onPressed: () => Navigator.pop(context),
-                  child: const Text('OK'),
-                )
-              ],
-            );
-          },
-        );
+      UserDetailViewModel viewModel = context.read<UserDetailViewModel>();
+      viewModel.gettingUsers();
+      List<UserDetail> users = viewModel.users;
+
+      for (int i = 0; i < users.length; i++) {
+        if (users[i].userEmail == email.text &&
+            users[i].password == password.text) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const NewsScreen(),
+            ),
+          );
+        }
+        return;
       }
+      wrongUserCredential();
     }
+  }
+
+  Future<void> wrongUserCredential() async {
+    await showDialog<String>(
+      context: context,
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: const Text('Login failed'),
+          content: const Text(
+              '''Your username or password are incorrect.\nPlease try again'''),
+          actions: [
+            CupertinoButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('OK'),
+            )
+          ],
+        );
+      },
+    );
   }
 }
